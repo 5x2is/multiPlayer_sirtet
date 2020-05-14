bc1cd880e5ec3dda322935b9450e3752b974f4e7
5x2is <gyroscope.blog@gmail.com>
2020-05-15 08:16:40 +0900

    クライアントサイドのクラス化

--- src/public/js/SharedSettings.js
+++ src/public/js/SharedSettings.js
@@ -1,4 +1,12 @@
 'use strict';
 class SharedSettings{
-    
+    static get FIELD_WIDTH(){
+        return 500;
+    }
+    static get FIELD_HEIGHT(){
+        return 500;
+    }
+}
+if(typeof module !=='undefined' && typeof module.exports !=='undefined'){
+    module.exports = SharedSettings;
 }
