9f09198da7199e47e20bec2a4b6ae1570044e358
5x2is <gyroscope.blog@gmail.com>
2020-05-21 18:49:00 +0900

    herokutest 3

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ console.log('ver1.0.1');
 //port関係
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 //パス指定用モジュール
 const path = require('path');
 //webSocket
-const io = require('socket.io').listen(app.listen(PORT));
+//const io = require('socket.io').listen(app.listen(PORT));
 //ゲームクラス
 const Game = require('./libs/game.js');
 
@@ -18,8 +18,9 @@ app.listen(PORT,()=>{
 	console.log('server listening port:'+PORT);
 });
 
+console.log('line21');
 const game = new Game();
-game.start(io);
+//game.start(io);
 
 //静的ファイルのルーティング
 app.use(express.static(path.join(__dirname,'public')));
